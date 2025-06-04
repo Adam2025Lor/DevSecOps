6c0bc20601553fec6152ee88f6acb1974cb2a975
Adam <adamlor1234@gmail.com>
2025-06-04 15:37:57 +0300

    new test security22

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 def home():
     return "Hello from DevSecOps!!!"
 
-
+# Dangerous route using eval()
+@app.route("/danger")
+def danger():
+    user_input = request.args.get("code")
+    return str(eval(user_input))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
