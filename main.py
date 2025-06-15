4cdd062a91eab34792b827fadb4d9dd2f62a5643
Adam <adamlor1234@gmail.com>
2025-06-15 15:53:21 +0300

    new

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevSecOps!!!"
+    return "Hello from DevSecOps!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
